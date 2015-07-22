de1b056eff065eedfbab9a48b65f85aa58b21e40
Rishi Gupta <rishig@mac.com>
2015-07-21 22:04:53 -0700

    Extract shape data and begin work on drawing

--- js/flowchart.js
+++ js/flowchart.js
@@ -1,5 +1,18 @@
-function fooFunc(foobar)
+function initFlowchart(data)
 {
+	var shapes = data.PageContents.Shapes.Shape;
+	
+	var rects = svg.selectAll("rect")
+	   .data(shapes)
+	   .enter()
+	   .append("rect");
+	   
+	  rects.attr("width", 10)
+	       .attr("height", 10)
+	       .attr("x", function(d, i) {
+		       return (i * 15);
+	       });
+	
 	var bar = [4, 5, 6, 7];
 	console.log(bar);
 }
