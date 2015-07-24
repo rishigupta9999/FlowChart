4d39a20704ef466f45d336e7a7df3da76b6c927f
Rishi Gupta <rishig@mac.com>
2015-07-23 22:15:21 -0700

    Begin working on extract more fields from the shapes

--- js/flowchart.js
+++ js/flowchart.js
@@ -6,11 +6,32 @@ function initFlowchart(data)
 	   .data(shapes)
 	   .enter()
 	   .append("rect");
+	
+	var svgWidth = svg.attr("width");
+	var svgHeight = svg.attr("height");
+	var rectWidth = 10;
+	var rectHeight = 10;
+	var cellsWidth = svgWidth / rectWidth;
+	var cellsHeight = svgHeight / rectHeight;
+	var scaleX = 100;
+	var scaleY = 20;
+	
+//		console.assert(false, "blah");
 	   
-	  rects.attr("width", 10)
-	       .attr("height", 10)
+	  rects.attr("width", rectWidth)
+	       .attr("height", rectHeight)
 	       .attr("x", function(d, i) {
-		       return (i * 15);
+		       var curShape = shapes[i];
+		       var cell = curShape.Cell[0];
+		       var name = cell["@N"];
+		       
+		       console.assert(name == "PinX", "Expected PinX");
+
+		       var posX = cell["@V"]
+		       		       console.log(posX);
+
+		       return posX * scaleX;
+		       
 	       });
 	
 	var bar = [4, 5, 6, 7];
